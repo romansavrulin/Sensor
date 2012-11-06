2e3bd566b72fc2bb9de9b9c65ed9ca4cfec57cff
Jochen Sprickerhof <jochen@sprickerhof.de>
2012-11-06 20:38:48 +0100

    Add support for XN sensor version 2
    taken from https://kforge.ros.org/openni/drivers

--- Source/XnDeviceSensorV2/XnDeviceSensorIO.cpp
+++ Source/XnDeviceSensorV2/XnDeviceSensorIO.cpp
@@ -30,6 +30,7 @@
 // Defines
 //---------------------------------------------------------------------------
 #define XN_SENSOR_VENDOR_ID			0x1D27
+#define XN_SENSOR_2_0_PRODUCT_ID	0x0200
 #define XN_SENSOR_5_0_PRODUCT_ID	0x0500
 #define XN_SENSOR_6_0_PRODUCT_ID	0x0600
 #define XN_SENSOR_6_0_1_PRODUCT_ID	0x0601
@@ -410,6 +411,11 @@ XnStatus XnSensorIO::EnumerateSensors(XnConnectionString* aConnectionStrings, Xn
 	// search for a v5.0 device
 	nRetVal = Enumerate(XN_SENSOR_VENDOR_ID, XN_SENSOR_5_0_PRODUCT_ID, devicesSet);
 	XN_IS_STATUS_OK(nRetVal);
+
+	// try searching for an older device
+	nRetVal = Enumerate(XN_SENSOR_VENDOR_ID, XN_SENSOR_2_0_PRODUCT_ID, devicesSet);
+	XN_IS_STATUS_OK(nRetVal);
+
 #endif
 	
 	// now copy back
